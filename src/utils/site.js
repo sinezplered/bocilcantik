c244f84e189f72e52bd527cb1c365b4912e7cc00
sinezplered <sinezplered@gmail.com>
2025-07-15 04:26:47 +0700

    Add files via upload

---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Bocil Cantik';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
+export const url = 'https://bocilcantik.pages.dev';
+export const terbit = '2025-01-28T00:01:03Z';
+
+export const google = 'URxJLGYKFTF1JHqPnZZpjeCVERLvWQeIppwnuddpxtk';
+export const yandex = 'e7a19b0f09ca422f'
+
+export const index = 'e1217121-cbec-4740-a6d8-992a70f2c7f6';
